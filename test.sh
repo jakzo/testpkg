caf40fcbac5f7e1466f2b12f47a9f69cfc14cd61
jakzo <jack@jf.id.au>
2021-04-23 22:20:13 +1000

    testing

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 unset npm_config_registry
-npm config get _authToken | head -c 5
+# npm config get _authToken | head -c 5
 npm publish /home/runner/work/testpkg/testpkg --json --access public --tag latest
